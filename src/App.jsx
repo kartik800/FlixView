f3f5579543f6582e98942c02e081e8e4a9b32040
Kartik <er.kartik18102016@gmail.com>
2023-06-07 14:01:18 +0530

    added explore and pagenotfound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css'
-import {useEffect } from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {fetchDataFromApi} from './utils/api'
+import "./App.css";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
 // import { useSelector, useDispatch } from 'react-redux'
-import { useDispatch } from 'react-redux'
-import { getApiConfiguration , getGenres} from './store/homeSlice';
+import { useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
-import Home from './pages/home/Home';
+import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Details from './pages/details/Details';
-import SearchResult from './pages/searchResult/SearchResult';
-import Explore from './pages/explore/Explore';
-import PageNotFound from './pages/404/PageNotFound';
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -22,53 +22,50 @@ function App() {
     fetchApiConfig();
     generesCall();
   }, []);
-  
+
   const fetchApiConfig = () => {
-    fetchDataFromApi('/configuration')
-      .then((res) => {
-        console.log(res);
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
 
-        const url = {
-          backdrop: res.images.secure_base_url + "original",
-          poster: res.images.secure_base_url + "original",
-          profile: res.images.secure_base_url + "original",
-        };
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
 
-        dispatch(getApiConfiguration(url));
-      });
+      dispatch(getApiConfiguration(url));
+    });
   };
 
   const generesCall = async () => {
-    let promises = []
+    let promises = [];
     let endPoints = ["tv", "movie"];
-    let allGeneres = {}
+    let allGeneres = {};
 
     endPoints.forEach((url) => {
-      promises.push(fetchDataFromApi(`/genre/${url}/list`))
-    })
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
 
     const data = await Promise.all(promises);
-    data.map(({genres}) => {
-      return genres.map((item) => (allGeneres[item.id] = item))
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGeneres[item.id] = item));
     });
 
     dispatch(getGenres(allGeneres));
-
-  }
+  };
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/:mediaType/:id' element={<Details/>}/>
-        <Route path='/search/:query' element={<SearchResult/>}/>
-        <Route path='/explore/:mediaType' element={<Explore/>}/>
-        <Route path='*' element={<PageNotFound/>}/>
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
-    <Footer/>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,10 @@ import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
 
-
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/logo.svg";
-
 
 const Header = () => {
   const [show, setShow] = useState("top");
@@ -21,94 +19,109 @@ const Header = () => {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
   }, [location]);
-  
 
   const controlNavbar = () => {
-    if(window.scrollY > 200){
-      if(window.scrollY > lastScrollY && !mobileMenu){
-        setShow("hide")
-      }else{
-        setShow("show")
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
       }
     } else {
       setShow("top");
     }
     setLastScrollY(window.scrollY);
-
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll", controlNavbar)
+    window.addEventListener("scroll", controlNavbar);
     return () => {
-      window.removeEventListener("scroll", controlNavbar)
-    }
-  }, [lastScrollY])
-  
-
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
 
   const openSearch = () => {
-      setMobileMenu(false);
-      setShowSearch(true);
-  }
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
   const openMobileMenu = () => {
-      setMobileMenu(true);
-      setShowSearch(false);
-
-  }
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
   const searchQueryHandler = (event) => {
-    if(event.key == "Enter" && query.length > 0){
-        navigate(`/search/${query}`); 
-        setTimeout(()=>{
-          setShowSearch(false);
-        },1000)
+    if (event.key == "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
     }
   };
 
   const navigationHandler = (type) => {
-      if(type === "movie"){
-        navigate("/explore/movie");
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
 
-      }else {
-          navigate("/explore/tv");
-      }
-
-      setMobileMenu(false);
-  }
-
+    setMobileMenu(false);
+  };
 
   return (
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
-        <div className="logo">
-          <img src={logo} alt=""/>
+        <div className="logo" onClick={() => navigate("/")}>
+          <img src={logo} alt="" />
         </div>
         <ul className="menuItems">
-          <li className="menuItem" onClick={()=> {navigationHandler("movie")}}>Movies</li>
-          <li className="menuItem" onClick={()=> {navigationHandler("tv")}}>TV Shows</li>
-          <li className="menuItem" onClick={openSearch}><HiOutlineSearch/></li>
+          <li
+            className="menuItem"
+            onClick={() => {
+              navigationHandler("movie");
+            }}
+          >
+            Movies
+          </li>
+          <li
+            className="menuItem"
+            onClick={() => {
+              navigationHandler("tv");
+            }}
+          >
+            TV Shows
+          </li>
+          <li className="menuItem" onClick={openSearch}>
+            <HiOutlineSearch />
+          </li>
         </ul>
         <div className="mobileMenuItems">
-          <HiOutlineSearch onClick={openSearch}/>
+          <HiOutlineSearch onClick={openSearch} />
           {mobileMenu ? (
-            <VscChromeClose onClick={()=> setMobileMenu(false)}/>
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
           ) : (
-            <SlMenu onClick={openMobileMenu}/>
+            <SlMenu onClick={openMobileMenu} />
           )}
-
         </div>
       </ContentWrapper>
-      { showSearch && (<div className="searchBar">
-        <ContentWrapper>
-          <div className="searchInput">
-              <input onChange={(e)=> setQuery(e.target.value)} onKeyUp={searchQueryHandler} type="text" placeholder="Search for a movie or tv show....."/>
-              <VscChromeClose onClick={()=> setShowSearch(false)}/>
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+                type="text"
+                placeholder="Search for a movie or tv show....."
+              />
+              <VscChromeClose onClick={() => setShowSearch(false)} />
             </div>
-        </ContentWrapper>
-      </div>)}
+          </ContentWrapper>
+        </div>
+      )}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
